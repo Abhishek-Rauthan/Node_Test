a6aa8e09f531631db7b914595dbd1e6a969eb31f
Abhishek-Rauthan <abhishekrauthan321@gmail.com>
2024-08-09 18:08:25 +0530

    added timestamps for user schema

--- src/users/user.schema.ts
+++ src/users/user.schema.ts
@@ -1,29 +1,35 @@
 import { model, Schema } from "mongoose";
 
-const userSchema = new Schema({
-  // user email
-  email: {
-    type: Schema.Types.String,
-    required: [true, "User must have an email"],
-  },
+const userSchema = new Schema(
+  {
+    // user email
+    email: {
+      type: Schema.Types.String,
+      required: [true, "User must have an email"],
+    },
 
-  // user name
-  name: {
-    type: Schema.Types.String,
-    required: [true, "User must have an name"],
-  },
+    // user name
+    name: {
+      type: Schema.Types.String,
+      required: [true, "User must have an name"],
+    },
 
-  // user password
-  password: {
-    type: Schema.Types.String,
-    required: [true, "User must have a password"],
-  },
+    // user password
+    password: {
+      type: Schema.Types.String,
+      required: [true, "User must have a password"],
+    },
 
-  age: {
-    type: Schema.Types.Number,
-    required: [false],
+    // user age (not required)
+    age: {
+      type: Schema.Types.Number,
+      required: [false],
+    },
   },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Users = model("Users", userSchema);
 
